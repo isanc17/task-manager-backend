b1651a518e7e04f0ca59ca41583fde4abf745c4e
isanc17 <isanc17@bancodebogota.com.co>
2024-10-23 13:44:05 -0500

    feat: validator a login

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,9 +1,11 @@
-import { Router } from 'express';
-import { login } from '../controllers/loginController';
+import { Router } from "express";
+import { login } from "../controllers/loginController";
+import { handleValidationErrors } from "../middleware/validationHandler";
+import { loginValidator } from "../middleware/loginValidator";
 
 const router = Router();
 
 // Ruta de login
-router.post('/login', login);
+router.get("/login", loginValidator, handleValidationErrors(true), login);
 
 export default router;
