f92df398accc6f540f87aefd6aa7799e89a71056
isanc17 <isanc17@bancodebogota.com.co>
2024-10-23 03:54:20 -0500

    ADD: main ts

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express, { Application, Request, Response } from "express";
+import bodyParser from "body-parser";
+import usuarioRoutes from "./routes/usuarioRoutes";
+import tareasRouter from "./routes/tareasRoutes";
+import loginRoutes from "./routes/loginRoutes";
+import { autenticarToken } from "./middleware/loginMiddleware";
+
+const app: Application = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(loginRoutes);
+app.use(autenticarToken);
+app.use("/usuarios", usuarioRoutes);
+app.use("/tasks", tareasRouter);
+
+app.use((req, res) => {
+  res.status(404).json({
+    status: "error",
+    message: "Ruta no encontrada.",
+  });
+});
+
+// Iniciar servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
